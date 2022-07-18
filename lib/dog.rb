5f833dfd24df69bff68c29d1696f93a394a33ed4
Kunj <shahkunj42@gmail.com>
2022-07-18 12:27:56 -0400

    commit tree b54d52f83fabbe71e4db94b6319843caf224be58

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed = breed 
+    end
+    def breed
+        @breed
+    end
 end
+
+# class Dog
+#     attr_accessor :name
+#     attr_accessor :breed
+#     def initialize(name, breed)
+#         @name = name
+#         @breed = breed
+#     end
+# end
